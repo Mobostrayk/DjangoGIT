08c1a567676018abc2fa8af11e252063e53f50c2
Елисей Чубукин <e-chubukin2017@yandex.ru>
2025-10-03 10:29:43 +0700

    feat: add author field to Post model

--- posts/tests.py
+++ posts/tests.py
@@ -2,18 +2,24 @@ from django.test import TestCase
 from .models import Post
 from django.utils import timezone
 
+from django.test import TestCase
+from .models import Post
+from django.utils import timezone
+
 class PostModelTest(TestCase):
 
     def setUp(self):
         self.post = Post.objects.create(
             title="Тестовый заголовок",
-            content="Тестовое содержимое"
+            content="Тестовое содержимое",
+            author="Тестовый автор"  
         )
 
     def test_post_creation(self):
         """Проверка, что пост создаётся корректно"""
         self.assertEqual(self.post.title, "Тестовый заголовок")
         self.assertEqual(self.post.content, "Тестовое содержимое")
+        self.assertEqual(self.post.author, "Тестовый автор") 
         self.assertTrue(self.post.created_at <= timezone.now())
 
     def test_post_str(self):
