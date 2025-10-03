efb1bab7872deea8b86fca530950dd351c5fd99e
Елисей Чубукин <e-chubukin2017@yandex.ru>
2025-10-03 10:44:38 +0700

    feat: add post list view with tests

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def home_view(request):
     return HttpResponse("<h1>Добро пожаловать в мой блог!</h1>")
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    html = "<h1>Все посты</h1><ul>"
+    for post in posts:
+        html += f"<li><strong>{post.title}</strong>: {post.content[:50]}...</li>"
+    html += "</ul>"
+    return HttpResponse(html)
 
 def about_view(request):
     return HttpResponse("<h1>Об авторе</h1><p>Привет! Я автор этого блога.</p>")
